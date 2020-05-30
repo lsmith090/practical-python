a1e8292aa9bb278c073f53309cafc5ce708f3104
lsmith90 <lsmith@medicusit.com>
2020-05-30 08:58:55 -0400

    more changes to 1_10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+#Base mortage info
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+
+#Extra payment variables
+extra_payment = 1000
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+
+#Counter variables
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months += 1
+    if months <= extra_payment_end_month and months >= extra_payment_start_month:
+        principal = principal - extra_payment
+        total_paid += extra_payment
+    #Print table
+    if principal < 0:
+        total_paid += principal
+        principal += abs(principal)
+
+    print(months, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Total months', months)
