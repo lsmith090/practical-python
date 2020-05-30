73b48c6d645a06c311d85e4c5b6d998bc9b7a0d6
lsmith90 <lsmith@medicusit.com>
2020-05-30 11:26:22 -0400

    added filename argument

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,25 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
 def portfolio_cost(filename):
     total_cost = 0
     with open(filename, 'rt') as f:
-        headers = next(f).split(',')
-        for line in f:
-            stock = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             try:
-                num_shares = int(stock[1])
+                num_shares = int(row[1])
             except ValueError:
-                print("Couldn't parse", line)
-
+                print("Couldn't parse", row)
             try:
-                cost_shares = float(stock[2])
+                cost_shares = float(row[2])
             except ValueError:
-                print("Couldn't parse", line)
-
+                print("Couldn't parse", row)
             total_cost += num_shares * cost_shares
-        return total_cost  
+    return total_cost  
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
