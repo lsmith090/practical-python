040fe245679bc38d3e2113aacbffecba1e1afeef
lsmith90 <lsmith@medicusit.com>
2020-05-30 11:10:38 -0400

    re-wrote to use function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,25 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            stock = line.split(',')
+            try:
+                num_shares = int(stock[1])
+            except ValueError:
+                print("Couldn't parse", line)
+
+            try:
+                cost_shares = float(stock[2])
+            except ValueError:
+                print("Couldn't parse", line)
+
+            total_cost += num_shares * cost_shares
+        return total_cost  
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
