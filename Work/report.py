a7edfc306c0978456b6f31f2142674fc2bfb4e6d
lsmith90 <lsmith@medicusit.com>
2020-06-07 11:22:58 -0400

    exercise 2.9

--- Work/report.py
+++ Work/report.py
@@ -29,7 +29,7 @@ def read_prices(filename):
                 prices[row[0]] = float(row[1])
     return prices
 
-def can_i_retire(prices, portfolio):
+def can_i_retire(portfolio, prices):
     value = 0.0
     delta_value = 0.0
     for holding in portfolio:
@@ -45,3 +45,17 @@ def can_i_retire(prices, portfolio):
             #print('Holding Name:', holding['name'], 'Original price:', og_price, 'Current shares held:', shares, "Original holding value:", (og_price*shares))
     print("Current value:", f'{delta_value:0.2f}')
     print("Gain/loss since purchase:", f'{delta_value-value:0.2f}')
+
+def make_report(portfolio, prices):
+    report = []
+    for holding in portfolio:
+        if prices.get(holding['name'], 0.0) > 0:
+            #set variables
+            
+            purchase_price = holding['price']
+            current_price = prices[holding['name']]
+            shares = holding['shares']
+            change =  current_price - purchase_price
+            report.append((holding['name'], shares, current_price, change))
+            
+    return report
