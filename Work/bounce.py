e7a03cf28557ca2c6b142634941830990cf2e687
Logan Smith <logansmith090@gmail.com>
2024-12-27 21:40:32 -0500

    fix bounce solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,9 +2,9 @@
 #
 # Exercise 1.5
 bounce_height = 100 #Meters
-bounce_count = 0
+bounce_count = 1
 
 while bounce_count <= 10:
-    bounce_height = bounce_height * (3/5)
+    bounce_height = round(bounce_height * (3/5), 4)
     print (bounce_count, round(bounce_height, 4))
     bounce_count += 1
